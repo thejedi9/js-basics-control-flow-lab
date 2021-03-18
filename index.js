b49206ce9c09a11048bb03b847fd821417a7606a
Jede <brahmajede@gmail.com>
2021-03-18 00:23:05 -0400

    commit tree 7c9a8df3f0a354adb6d57f26b71033c72e367909

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+   return `This one is on me!`; 
+  } 
+  else if (1999 < feet && feet < 2500) {
+    return `I will gladly take your thirty bucks.`;
+  } 
+  else  {
+    return `No can do.`;
+  }
+  
+}
+//const destination = `city`;
+//const city = destination === `NYC` ? `Ok, sounds good.` : `No go`;
+
+function ternaryCheckCity(destination){
+ return destination === 'NYC' ? `Ok, sounds good.` : `No go.`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case `generous` :
+      return `Thank you so much.`;
+    case `not as generous` :
+      return `Thank you.`;
+    default :
+      return `Bye.`
+  }
 }
